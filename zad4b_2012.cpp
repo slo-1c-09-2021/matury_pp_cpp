d2c49808612d991b915f0dc1ce7822d4026ade67
slo-1c-09-2021 <81371213+slo-1c-09-2021@users.noreply.github.com>
2022-03-30 08:31:46 +0200

    dont

--- zad4b_2012.cpp
+++ zad4b_2012.cpp
@@ -4,6 +4,8 @@ int main()
 {
     ifstream plik("cyfry.txt");
     string str;
+    string najwliczba;
+    string najmliczba;
     int suma;
     int najwieksza;
     int najmniejsza = 9999999;
@@ -11,13 +13,23 @@ int main()
     {
         for(int i=0; i<str.size(); i++)
         {
-            suma +=  str[i];
+            suma += (int)str[i] - '0';
         }
-        if(suma>najwieksza) najwieksza = suma;
-        if(suma<najmniejsza) najmniejsza = suma;
+        if(suma>najwieksza)
+        {
+            najwieksza = suma;
+            najwliczba =  str;
+        }
+        if(suma<najmniejsza)
+        {
+            najmniejsza = suma;
+            najmliczba = str;
+        }
         suma = 0;
     }
     cout << najwieksza << endl;
-    cout << najmniejsza;
+    cout << najwliczba << endl;
+    cout << najmniejsza << endl;
+    cout << najmliczba;
 
 }
